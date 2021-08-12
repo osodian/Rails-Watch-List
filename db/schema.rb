c0408c36310f5ae79a242ee4135a3c9fdc98458f
Oscar Sodian <oscar.sodian@gmx.de>
2021-08-12 14:46:40 +0200

    set up

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_08_12_124023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookmarks", force: :cascade do |t|
+    t.text "comment"
+    t.bigint "movies_id", null: false
+    t.bigint "lists_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["lists_id"], name: "index_bookmarks_on_lists_id"
+    t.index ["movies_id"], name: "index_bookmarks_on_movies_id"
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.text "overview"
+    t.text "poster_url"
+    t.integer "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "bookmarks", "lists", column: "lists_id"
+  add_foreign_key "bookmarks", "movies", column: "movies_id"
 end
