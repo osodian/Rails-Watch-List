0e7683fff0eb533dd73d0e919f698b49e9c5500f
Oscar Sodian <oscar.sodian@gmx.de>
2021-08-12 15:59:37 +0200

    Moin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_12_133910) do
+ActiveRecord::Schema.define(version: 2021_08_12_124023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,8 @@ ActiveRecord::Schema.define(version: 2021_08_12_133910) do
     t.bigint "list_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["list_id"], name: "index_bookmarks_on_list_id"
-    t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
+    t.index ["list_id"], name: "index_bookmarks_on_lists_id"
+    t.index ["movies_id"], name: "index_bookmarks_on_movies_id"
   end
 
   create_table "lists", force: :cascade do |t|
@@ -40,6 +40,6 @@ ActiveRecord::Schema.define(version: 2021_08_12_133910) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "bookmarks", "lists"
-  add_foreign_key "bookmarks", "movies"
+  add_foreign_key "bookmarks", "lists", column: "lists_id"
+  add_foreign_key "bookmarks", "movies", column: "movies_id"
 end
